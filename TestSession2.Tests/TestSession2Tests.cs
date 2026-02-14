7ba7ae74b8ab661c14e58048339b3fb1c52542db
Dmitry <belovdima1122@yandex.ru>
2026-02-14 12:33:20 +0300

    Добавил проект с тестами

---
+++ TestSession2.Tests/TestSession2Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace TestSession2.Tests
+{
+    public class TestSession2Tests
+    {
+        [Fact]
+        public void SalaryCalculation_ValiData_ReturnsCorrectValue()
+        {
+            // Arrange
+            var calculation = new Calculation();
+            double bet = 1;
+            string post = "директор";
+
+            // Act
+            double salary = calculation.SalaryCalculation(post, bet);
+
+            // Assert
+            Assert.Equal(salary, 26100);
+        }
+    }
+}
